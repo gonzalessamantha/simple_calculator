c79d34a548671c2500cf5c86b855927790faa2fe
unknown <gonzalessamanthagrace07@gmail.com>
2026-05-04 02:42:01 +0800

    I added input and result grid to input and show the answer from the given calculation

--- calculator.py
+++ calculator.py
@@ -1,8 +1,19 @@
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+calculation = ()
 
-root.title("Simple Calculator ng Maangas")
-root.geometry('350x450')
+def add_to_calc(symbol):
+    pass
 
+def evaluate_symbol(symbol):
+    pass
+
+def clear_field(symbol):
+    pass
+
+root = tk.Tk()
+root.geometry("350x450")
+
+text_result = tk.Text(root, width=20, height=2, font=("Arial", 24))
+text_result.grid(columnspan=5)
 root.mainloop()
